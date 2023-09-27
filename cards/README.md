8ebf23f552aecdba4279eced6d6bde013244ba20
Thomas Laurenson <thomas@thomaslaurenson.com>
2023-09-28 11:44:32 +1300

    Added Docker env for github-stats

--- cards/README.md
+++ cards/README.md
@@ -1,36 +1,29 @@
 # thomaslaurenson > cards
 
-Instead of using an API service, all GitHub cards are static and updated periodically.
+Instead of using an API service, all GitHub cards are static SVG files and updated periodically.
 
-If you want to use my setup:
+## Overview
 
-- Change the `username` or `user` parameter in the `fetch_cards.sh` script
-- Download and start the `github-readme-stats` API
-- Run the script using `./fetch_cards.sh`
+The stats and top languages cards are made using the [github-readme-stats](https://github.com/anuraghazra/github-readme-stats) project. This project is run locally using a Docker container - as this allows the use of my own GitHub access token, and access to stats from my private repos.
+
+The streak card is made using the [github-readme-streak-stats](https://github.com/DenverCoder1/github-readme-streak-stats).
+
+## Prerequisites
+
+- Bash, or similar shell
+- Docker
+- Curl
 
 ## Quickstart
-
-The stats and top languages cards are made using the [github-readme-stats](https://github.com/anuraghazra/github-readme-stats) project. Using a local copy, so I can use my own GitHub access token and, therefore, get data from my private repos.
 
 - Create token in GitHub
     - Settings > Developer Settings > Personal access tokens
     - Set the following permissions
     - `repo`: all
     - `user`: all
-- Download and start API
-
-```
-git clone https://github.com/anuraghazra/github-readme-stats.git
-cd github-readme-stats
-npm install express
-npm install
-PAT_1=TOKEN node express.js
-```
-
-The streak card is made using the [github-readme-streak-stats](https://github.com/DenverCoder1/github-readme-streak-stats). I use their API directly.
-
-To refresh all cards:
-
-```
-./fetch_cards.sh
-```
+- Clone repo
+    - `git clone https://github.com/thomaslaurenson/thomaslaurenson.git`
+    - `cd thomaslaurenson/cards`
+- Put token in: `github-readme-syats/.env` file
+- Change the `USERNAME` variable in the `fetch_cards.sh` script
+- Run the script using `./fetch_cards.sh`
--- cards/fetch_cards.sh
+++ cards/fetch_cards.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
 
+# Set username
+USERNAME="thomaslaurenson"
+
 # Move to script folder
 DIRECTORY=$(dirname "$0")
 cd $DIRECTORY
 
-# Query local API for GitHub stats cards
-curl "http://localhost:9000/?username=thomaslaurenson&count_private=true&hide_border=true&bg_color=00000000&title_color=FFFFFF&text_color=FFFFFF&show_icons=true&icon_color=FF8300&ring_color=FF8300" -o github_stats_card_dark.svg
+# Start github-readme-stats Docker container
+echo "[*] Building github-readme-stats Docker environment..."
+PROJECT="github-readme-stats"
+cd "$PROJECT"
+source .env
+docker image build -t "$PROJECT" --build-arg PAT_1=$PAT_1 .
+docker run --name "$PROJECT" --env-file .env -p 9000:9000 -d "$PROJECT"
 
-curl "http://localhost:9000/?username=thomaslaurenson&count_private=true&hide_border=true&bg_color=FFFFFFFF&title_color=000000&text_color=000000&show_icons=true&icon_color=FF8300&ring_color=FF8300" -o github_stats_card_light.svg
+echo "[*] Waiting for API to be available..."
+sleep 10
+
+# Query local API for GitHub stats cards
+echo "[*] Fetching github_stats_card..."
+curl "http://localhost:9000/?username=$USERNAME&count_private=true&hide_border=true&bg_color=00000000&title_color=FFFFFF&text_color=FFFFFF&show_icons=true&icon_color=FF8300&ring_color=FF8300" -o ../github_stats_card_dark.svg
+
+curl "http://localhost:9000/?username=$USERNAME&count_private=true&hide_border=true&bg_color=FFFFFFFF&title_color=000000&text_color=000000&show_icons=true&icon_color=FF8300&ring_color=FF8300" -o ../github_stats_card_light.svg
 
 # Query local API for GitHub top languages cards
-curl "http://localhost:9000/top-langs?username=thomaslaurenson&count_private=true&hide=tex,html&layout=donut&hide_border=true&bg_color=00000000&title_color=FFFFFF&text_color=FFFFFF" -o github_languages_card_dark.svg
+echo "[*] Fetching languages_card..."
+curl "http://localhost:9000/top-langs?username=$USERNAME&count_private=true&hide=tex,html&layout=donut&hide_border=true&bg_color=00000000&title_color=FFFFFF&text_color=FFFFFF" -o ../github_languages_card_dark.svg
 
-curl "http://localhost:9000/top-langs?username=thomaslaurenson&count_private=true&hide=tex,html&layout=donut&hide_border=true&bg_color=FFFFFF&title_color=000000&text_color=000000" -o github_languages_card_light.svg
+curl "http://localhost:9000/top-langs?username=$USERNAME&count_private=true&hide=tex,html&layout=donut&hide_border=true&bg_color=FFFFFF&title_color=000000&text_color=000000" -o ../github_languages_card_light.svg
+
+# Stop github-readme-stats Docker container and delete everything
+echo "[*] Destroying github-readme-stats Docker environment..."
+docker container stop github-readme-stats
+docker container rm github-readme-stats
+docker image rm github-readme-stats
+cd ..
 
 # Query streak API
-curl "https://streak-stats.demolab.com?user=thomaslaurenson&theme=dark&hide_border=true&date_format=M%20j%5B%2C%20Y%5D&exclude_days=Sun%2CSat&background=00000000" -o github_streak_card_dark.svg
+echo "[*] Fetching streak_card..."
+curl "https://streak-stats.demolab.com?user=$USERNAME&theme=dark&hide_border=true&date_format=M%20j%5B%2C%20Y%5D&exclude_days=Sun%2CSat&background=00000000" -o github_streak_card_dark.svg
 
-curl "https://streak-stats.demolab.com?user=thomaslaurenson&theme=default&hide_border=true&date_format=M%20j%5B%2C%20Y%5D&exclude_days=Sun%2CSat&background=00000000" -o github_streak_card_light.svg
+curl "https://streak-stats.demolab.com?user=$USERNAME&theme=default&hide_border=true&date_format=M%20j%5B%2C%20Y%5D&exclude_days=Sun%2CSat&background=00000000" -o github_streak_card_light.svg
